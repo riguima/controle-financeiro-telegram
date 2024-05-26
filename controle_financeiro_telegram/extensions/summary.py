011232fb507e91594fe4ea6c345a5998af05e8bc
riguima <richard.alexsander.guima@gmail.com>
2024-05-26 16:59:06 -0300

    feat: adicionado data de registro

--- controle_financeiro_telegram/extensions/summary.py
+++ controle_financeiro_telegram/extensions/summary.py
@@ -13,12 +13,16 @@ def init_bot(bot, start):
         table.align['Total'] = 'c'
         table.align['Restante'] = 'c'
         table.align['Último Recebimento'] = 'c'
+        table.align['Data de registro'] = 'c'
         with Session() as session:
             for project in session.scalars(select(Project)).all():
                 remaining_value = project.valor_total - sum([r.valor for r in project.recebimentos])
                 if remaining_value > 0:
                     sorted_receipts = sorted(project.recebimentos, key=lambda r: r.data)
-                    table.add_row([project.cliente.nome, f'R$ {project.valor_total:.2f}'.replace('.', ','), f'R$ {remaining_value:.2f}'.replace('.', ','), f'{sorted_receipts[-1].data:%d/%m/%Y}'])
+                    try:
+                        table.add_row([project.cliente.nome, f'R$ {project.valor_total:.2f}'.replace('.', ','), f'R$ {remaining_value:.2f}'.replace('.', ','), f'{sorted_receipts[-1].data:%d/%m/%Y}', f'{project.data}:%d/%m/%Y'])
+                    except IndexError:
+                        table.add_row([project.cliente.nome, f'R$ {project.valor_total:.2f}'.replace('.', ','), f'R$ {remaining_value:.2f}'.replace('.', ','), 'Sem recebimento', f'{project.data}:%d/%m/%Y'])
         bot.send_message(callback_query.message.chat.id, f'```{table}```', reply_markup=quick_markup({
             'Voltar': {'callback_data': 'return_to_main_menu'}
         }), parse_mode='MarkdownV2')
